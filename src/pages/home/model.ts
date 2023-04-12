25039edcc9ef55b5abcf7958e23e58d1c8228ee1
7iomka <7iomka@gmail.com>
2023-04-12 23:04:10 +0300

    Misc

--- src/pages/home/model.ts
+++ src/pages/home/model.ts
@@ -7,9 +7,9 @@ export const pageStarted = createEvent()
 const $demo = createStore(1)
 
 const $demo2 = createStore('')
-// if clock is not page event - scope not changes
+
 sample({ clock: $$navigation.$url, target: $demo2 })
-// if clock is pageStarted - scrope changes once clientside
+
 sample({ clock: pageStarted, source: $demo, fn: (v) => v + 1, target: $demo })
 
 debug({ $demo, $demo2 })
